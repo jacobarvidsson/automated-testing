f83417e53955d7d713d2655665f66c77603bf6ca
Jacob Arvidsson <jacobarvidsson@jacobs-air.lan>
2022-08-30 21:25:35 +0100

    chore: added reassure

---
+++ jest.unit-config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  preset: "@testing-library/react-native",
+  testPathIgnorePatterns: ["/node_modules/|/tests/e2e/|perf-test"],
+};
--- src/__tests__/Main.perf-test.tsx
+++ src/__tests__/Main.perf-test.tsx
@@ -1,22 +1,11 @@
-import { screen, fireEvent } from "@testing-library/react-native";
+import { screen } from "@testing-library/react-native";
 import { measurePerformance } from "reassure";
 import { Main } from "../Main";
 
 jest.setTimeout(60_000);
 test("Async Component", async () => {
   const scenario = async () => {
-    const button = screen.getByTestId("app.container");
-
-    // fireEvent.press(button);
-    // await screen.findByText("Count: 1");
-
-    // fireEvent.press(button);
-    // await screen.findByText("Count: 2");
-
-    // fireEvent.press(button);
-    // fireEvent.press(button);
-    // fireEvent.press(button);
-    // await screen.findByText("Count: 5");
+    screen.getByTestId("app.container");
   };
 
   await measurePerformance(<Main />, { scenario });

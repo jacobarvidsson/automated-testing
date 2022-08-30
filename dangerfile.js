40ff03010c3b0c43a8633f05caecfa575c3fae13
Jacob Arvidsson <jacobarvidsson@jacobs-air.lan>
2022-08-30 21:21:17 +0100

    chore: changed workflow

--- dangerfile.js
+++ dangerfile.js
@@ -1,19 +1,23 @@
-import {danger, warn} from 'danger'
+import { danger, warn } from "danger";
+import path from "path";
+import { dangerReassure } from "reassure";
 
-  
 // No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
+  warn("Please include a description of your PR changes.");
 }
-
 
 // Request changes to src also include changes to tests.
-const allFiles = danger.git.modified_files.concat(danger.git.created_files)
-const hasAppChanges = allFiles.some(p => includes(p, 'src/'))
-const hasTestChanges = allFiles.some(p => includes(p, '__tests__/'))
+const allFiles = danger.git.modified_files.concat(danger.git.created_files);
+const hasAppChanges = allFiles.some((p) => includes(p, "src/"));
+const hasTestChanges = allFiles.some((p) => includes(p, "__tests__/"));
 
 if (hasAppChanges && !hasTestChanges) {
-  warn('This PR does not include changes to tests, even though it affects app code.');
+  warn(
+    "This PR does not include changes to tests, even though it affects app code."
+  );
 }
 
-  
+dangerReassure({
+  inputFilePath: path.join(__dirname, ".reassure/output.md"),
+});
